ddc3a3274fd7eb5fabf7bc34539a8b0196259274
Hirikols <nbobyliakwork@gmail.com>
2021-08-14 16:16:13 +0300

    startup

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -41,7 +41,7 @@ namespace SoftServe.ITAcademy.BackendDubbingProject.Web
                     builder =>
                     {
                         builder
-                            .WithOrigins("10.26.0.168:3000","http://10.26.0.168:3000")
+                            .WithOrigins("10.26.0.168:3000", "http://10.26.0.168:3000")
                             .AllowAnyMethod()
                             .AllowAnyHeader()
                             .AllowCredentials();
