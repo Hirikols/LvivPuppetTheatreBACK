775f867480fef616c306641c40a4c3df45a476a5
Hirikols <nbobyliakwork@gmail.com>
2021-08-28 15:49:05 +0300

    test

--- check_service.sh
+++ check_service.sh
@@ -5,7 +5,7 @@
 
 while :
 do 
-    if [ $(curl -I 10.26.0.173:6222 | head -n1 | cut  -d " " -f 2) == 200 ]; then 
+    if [[ $(curl -I 10.26.0.173:6222 | head -n1 | cut  -d " " -f 2) == '200' ]]; then 
         break
         sleep 1
     fi
